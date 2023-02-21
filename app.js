9bb4d4d5c23cfd5b2e6fd1b06720f33364e2d104
rtg <120173063+rtgfba@users.nonreply.github.com>
2023-02-21 18:29:18 -0500

    Parse input data to output city

--- app.js
+++ app.js
@@ -1,30 +1,36 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 const app = express();
 
 
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.get("/", function(req,res){
-    var city = "Silver Spring"
-    const url = "https://api.weatherapi.com/v1/current.json?key=0e9af79924c94c8988b190520232102&q=" + city + "&aqi=no"
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req,res){
+    const city = req.body.cityName
+    const apiKey = "0e9af79924c94c8988b190520232102&q"
+    const url = "https://api.weatherapi.com/v1/current.json?key=" + apiKey  + "=" + city + "&aqi=no"
 
     https.get(url, function(response){
-        console.log(response.statusCode);``
+        console.log(response.statusCode);
         response.on("data", function(data){
            const weatherData = JSON.parse(data);
            const temp = weatherData.current.temp_f
            const weatherDescription = weatherData.current.condition.text
            const weatherIcon = weatherData.current.condition.icon
-           console.log(weatherData);
+           console.log();
            res.write("<h1>The temperature in " + city + ", is " + temp + " degrees Fahrenheit</h1>");
            res.write("<h2>The weather is currently " + weatherDescription + "</h2>");
            res.write("<img src=" + weatherIcon + ">");
            res.send();
         });
     });
- 
+
 });
-
-
 
 
 
